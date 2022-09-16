bec8722382cf365147e7c04efbc05ae0c1fee3e1
EjiroLaurelD <Dafelaurele@gmail.com>
2022-09-16 19:42:14 +0100

    weather report

--- forweather.sh
+++ forweather.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 date=$(date)
+
 for x in $(cat cities.txt)
-do 
-	weather=$(curl -s http://wttr.in/$x?format=3)
-	echo "the weather for $weather and the date is $date  "
+do 	
+	weather=$(curl -s  http://wttr.in/$x?format=3)
+	echo "the weather for $weather "
 done
+
+
+	
